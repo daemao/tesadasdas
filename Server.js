0145cd7cea70f14dd80495e688e057fd9aebcd08
Daemao <yersultan.nagashtay@nu.edu.kz>
2019-03-17 08:46:05 +0600

    sadsada

--- Server.js
+++ Server.js
@@ -5,11 +5,12 @@ var detectCharset = require('detect-charset');
 
 //HABR берет топ посты за предыдущий месяц
 const   habr_url = "https://habr.com/ru/top/monthly/"
+const   habr_url_incr = "https://habr.com/ru/top/"
 const Iconv = require ("iconv");
 var Buffer = require('buffer').Buffer;
 //пикабу берет топ посты за предыдущий месяц по категории айти
 const   pikabu_url = "https://pikabu.ru/tag/IT?d=4065&D=4093";
-
+const   pikabu_url_incr = "https://pikabu.ru/tag/IT?d=4092&D=4093";
 setInterval(function() {
     var total_tags = [];
 	// HABR request
@@ -55,7 +56,79 @@ setInterval(function() {
 	    }
 	});
 
+
+	request(habr_url_incr, function (error, response, body) {
+	    if (!error) {
+	         var $ = cheerio.load(body);
+	         var elems = [];
+	        
+	         posts = $("div.posts_list article ")
+	         .each(function (index, element) {
+	         		let $$ = cheerio.load(element);
+	         		var likes = 0;
+	         		var shares = 0;
+	         		var views = 0;
+	         		var comments = 0;
+	         		$$("footer.post__footer ul li ").each((i,e)=>{
+	         				let temp = $$(e).children().children().text();
+	         				let int = temp.replace("k","000").replace("M","000000").replace(",","");
+	         				int = int*1;
+	         				if(i == 1) likes=int;
+	     					if(i == 2) shares=int;
+	 						if(i == 3) views=int;
+							if(i == 4) comments=int;
+	         			});
+	         		let total = likes+shares+views+comments;
+
+	         		$$("ul.post__hubs li a").each((i,hub)=>{
+	         			
+	         			let topic = $$(hub).text()
+	         			console.log("aaaaa2",detectCharset(topic));
+	         			if (total_tags.filter(e=>e.name == topic).length == 0){
+	         				total_tags.push({name:topic,value:total});
+	         			}else{
+	         				total_tags.forEach(e=>{
+	         					if(e.name == topic)e.value = e.value+total;
+	         				})
+	         			} 
+	         		})
+			})
+
+	    } else {
+	        console.log("Произошла ошибка: " + error);
+	    }
+	});
+
 	request(pikabu_url, function (error, response, body) {
+	    if (!error) {
+	        var $ = cheerio.load(body);
+	        $("article.story").each((i,e)=>{
+	        	var likes = 0;
+	         	let $$ = cheerio.load(e);
+	         	likes = $$("div.story__rating-count").text()*1;
+	         	$$("a.tags__tag").each((i,hub)=>{
+	         			let  topic =$$(hub).text();
+	         			console.log("c",detectCharset(topic));
+	         			var iconv = new Iconv.Iconv("windows-1251", 'utf-8');
+						x = iconv.convert(topic);
+
+						topic = x.toString();
+						console.log("c2",detectCharset(topic));
+	         			if (total_tags.filter(e=>e.name == topic).length == 0){
+	         				total_tags.push({name:topic,value:likes});
+	         			}else{
+	         				total_tags.forEach(e=>{
+	         					if(e.name == topic)e.value = e.value+likes;
+	         				})
+	         			} 
+	         		})
+
+	        })
+	    } else {
+	        console.log("Произошла ошибка: " + error);
+	    }
+	});
+	request(pikabu_url_incr, function (error, response, body) {
 	    if (!error) {
 	        var $ = cheerio.load(body);
 	        $("article.story").each((i,e)=>{
